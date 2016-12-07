a87c1cb990e92a5b9fd29257100628cff3d77ae1
jraw@uwo.ca <jraw@uwo.ca>
2016-12-07 10:04:11 -0500

    added allow delete

--- server.js
+++ server.js
@@ -23,6 +23,13 @@ var app = express();
 
 var userCollection; //Hopefully this works
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+})
 
 //View Engine
 //app.set('client', path.join(__dirname,'client'));
